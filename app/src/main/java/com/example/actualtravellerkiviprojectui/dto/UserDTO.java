ed4a240b153b605061673ca6288616be828713a9
GünlükGüneşlik <caglagunesocakli@gmail.com>
2025-04-24 21:58:10 +0300

    Profile a başlayacaktım ama kafamda bazı şeyler oturmadı.
    O yüzden beklemeye geçtim.

--- app/src/main/java/com/example/actualtravellerkiviprojectui/dto/UserDTO.java
+++ app/src/main/java/com/example/actualtravellerkiviprojectui/dto/UserDTO.java
@@ -1,4 +1,10 @@
 package com.example.actualtravellerkiviprojectui.dto;
 
+import java.util.ArrayList;
+
 public class UserDTO {
+    private int image;
+    private String userName;
+    ArrayList<String> userLanguages;
+
 }
