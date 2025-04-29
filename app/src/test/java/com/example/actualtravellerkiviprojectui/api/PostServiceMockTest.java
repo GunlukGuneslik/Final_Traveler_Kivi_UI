17e7ab7b3d7cfa216f369881397e160ff0e021e7
Yusuf Said Aktan <ysaktan@proton.me>
2025-04-29 12:48:55 +0300

    test(Post): migrate to Retrofit Types

--- app/src/test/java/com/example/actualtravellerkiviprojectui/api/PostServiceMockTest.java
+++ app/src/test/java/com/example/actualtravellerkiviprojectui/api/PostServiceMockTest.java
@@ -1,14 +1,18 @@
 package com.example.actualtravellerkiviprojectui.api;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import java.io.IOException;
 
 public class PostServiceMockTest {
 
     @Test
     public void fetchPosts() {
-        System.err.println(new PostServiceMock().fetchPosts(1,10,"").get(0).getBody());
+        try {
+            System.err.println(new PostServiceMock().fetchPosts(1, 10, "").execute().body().get(0).getBody());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
