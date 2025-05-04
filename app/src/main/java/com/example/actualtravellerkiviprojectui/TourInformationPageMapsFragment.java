1237219615456cfb76929caf1b39f676adb0c340
GünlükGüneşlik <caglagunesocakli@gmail.com>
2025-05-04 23:48:13 +0300

    Çok azıcık daha değişiklik
    Şimdi tour info page de haritada bir noktaya basıldığında o placin adı çıkıyor.

--- app/src/main/java/com/example/actualtravellerkiviprojectui/TourInformationPageMapsFragment.java
+++ app/src/main/java/com/example/actualtravellerkiviprojectui/TourInformationPageMapsFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.actualtravellerkiviprojectui.dto.PlaceModel;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,43 +16,57 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 
-public class TourInformationPageMapsFragment extends Fragment {
+public class TourInformationPageMapsFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     ArrayList<PlaceModel> placesOnTheTour;
     private GoogleMap mMap;
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            mMap = googleMap;
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * In this case, we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to
+     * install it inside the SupportMapFragment. This method will only be triggered once the
+     * user has installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-            if (placesOnTheTour.isEmpty()) {
-                LatLng defaultLocation =  new LatLng(39.925533, 32.866287);
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, 10));
-            } else {
+        if (placesOnTheTour.isEmpty()) {
+            LatLng defaultLocation =  new LatLng(39.925533, 32.866287);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, 10));
+        } else {
 
-                // Adding all available markers
-                for (PlaceModel place : placesOnTheTour) {
-                    mMap.addMarker(new MarkerOptions().position(place.getLocation()).title(place.getPlaceName()));
-                }
+            // Adding all available markers
+            for (PlaceModel place : placesOnTheTour) {
+                mMap.addMarker(new MarkerOptions().position(place.getLocation()).title(place.getPlaceName()));
             }
-
-            mMap.getUiSettings().setZoomControlsEnabled(true);
         }
-    };
+
+        mMap.setOnMarkerClickListener(this);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+        LatLng clickedLocation = marker.getPosition();
+
+        for (PlaceModel place : placesOnTheTour) {
+            if (place.getLocation().equals(clickedLocation)) {
+                Toast.makeText(getContext(), place.getPlaceName(), Toast.LENGTH_SHORT).show();
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(clickedLocation, 10));
+                return true;
+            }
+        }
+        return false;
+    }
 
     @Nullable
     @Override
@@ -70,7 +85,7 @@ public class TourInformationPageMapsFragment extends Fragment {
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+            mapFragment.getMapAsync(this);
         }
     }
 
