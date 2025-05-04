800f34c78a8bc806550a0d384eff63bca83cae06
Yusuf Said Aktan <ysaktan@proton.me>
2025-05-05 00:09:54 +0300

    feat(Post): add service methods

--- app/src/main/java/com/example/actualtravellerkiviprojectui/api/PostService.java
+++ app/src/main/java/com/example/actualtravellerkiviprojectui/api/PostService.java
@@ -1,16 +1,31 @@
 package com.example.actualtravellerkiviprojectui.api;
 
 import com.example.actualtravellerkiviprojectui.dto.PostDTO;
+import com.example.actualtravellerkiviprojectui.dto.UserDTO;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface PostService {
-    @GET("posts/feed")
-    Call<List<PostDTO>> fetchPosts(int page, int size, String sort);
+    @GET("posts/feed/{userId}")
+    Call<List<PostDTO>> fetchFeed(@Path("userId") int userId, @Query("page") int page, @Query("size") int size, @Query("sort") String sort);
+
+    @GET("posts/{postId}/get")
+    Call<PostDTO> fetchPost(@Path("postId") int postId, int userId);
+
+    @GET("posts/{postId}/like")
+    Call<PostDTO> likePost(@Path("postId") int postId, int userId);
+
+    @GET("posts/{postId}/unlike")
+    Call<PostDTO> unlikePost(@Path("postId") int postId, int userId);
+
+    @GET("posts/{postId}/likers")
+    Call<List<UserDTO>> likers(@Path("postId") int postId);
 
     @GET("posts/create")
     Call<PostDTO> createPost(@Body PostDTO post);
