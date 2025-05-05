58d9635db6fa481cb25cf08544f2950758b3d42d
Yusuf Said Aktan <ysaktan@proton.me>
2025-05-05 14:39:37 +0300

    feat(User): add service methods

--- app/src/main/java/com/example/actualtravellerkiviprojectui/api/UserService.java
+++ app/src/main/java/com/example/actualtravellerkiviprojectui/api/UserService.java
@@ -1,5 +1,6 @@
 package com.example.actualtravellerkiviprojectui.api;
 
+import com.example.actualtravellerkiviprojectui.dto.ImageDTO;
 import com.example.actualtravellerkiviprojectui.dto.UserDTO;
 
 import java.util.List;
@@ -8,6 +9,8 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface UserService {
     @GET("users/")
@@ -16,5 +19,19 @@ public interface UserService {
     @POST("users/")
     Call<UserDTO> createUser(@Body UserCreateDTO userCreateDTO);
 
+    @GET("users/{userId}/avatar")
+    Call<ImageDTO> getAvatarOfUser(@Path("userId") int userId);
+
+    @GET("users/type/{userType}")
+    Call<List<UserDTO>> getUsersByType(@Path("userType") UserDTO.UserType userType);
+
+    @GET("users/{userId}/followers")
+    Call<List<UserDTO>> getFollowersOfUser(@Path("userId") int userId);
+
+    @GET("users/{userId}/followed")
+    Call<List<UserDTO>> getUserFollowed(@Path("userId") int userId);
+
+    @GET("users/{userId}/follow")
+    Call<UserDTO> followUser(@Path("userId") int userId, @Query("targetUserId") int targetUserId);
 
 }
