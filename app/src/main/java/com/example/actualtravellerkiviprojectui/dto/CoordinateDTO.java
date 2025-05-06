93b8e6ac4bcf0c95946e6130f4a4fb2e97eebca5
Yusuf Said Aktan <ysaktan@proton.me>
2025-05-06 09:06:40 +0300

    feat(Event): add EventLocationDTO and CoordinateDTO

---
+++ app/src/main/java/com/example/actualtravellerkiviprojectui/dto/CoordinateDTO.java
@@ -0,0 +1,9 @@
+package com.example.actualtravellerkiviprojectui.dto;
+
+public class CoordinateDTO {
+    public Integer id;
+
+    public double latitude;
+    public double longtitude;
+
+}
--- app/src/main/java/com/example/actualtravellerkiviprojectui/dto/EventLocationDTO.java
+++ app/src/main/java/com/example/actualtravellerkiviprojectui/dto/EventLocationDTO.java
@@ -1,4 +1,11 @@
 package com.example.actualtravellerkiviprojectui.dto;
 
+import java.util.Set;
 public class EventLocationDTO {
+    public Integer id;
+    public String title;
+    public String description;
+    public Set<String> keywords;
+    public CoordinateDTO location;
+
 }
