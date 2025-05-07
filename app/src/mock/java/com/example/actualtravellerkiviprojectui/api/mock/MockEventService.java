acfe7b9e223508cb282fa0557de79decc89b9658
Yusuf Said Aktan <ysaktan@proton.me>
2025-05-07 21:46:56 +0300

    feat(Event): add mock service methods

--- app/src/mock/java/com/example/actualtravellerkiviprojectui/api/mock/MockEventService.java
+++ app/src/mock/java/com/example/actualtravellerkiviprojectui/api/mock/MockEventService.java
@@ -4,6 +4,7 @@ import com.example.actualtravellerkiviprojectui.api.EventService;
 import com.example.actualtravellerkiviprojectui.dto.EventCreateUpdate;
 import com.example.actualtravellerkiviprojectui.dto.EventDTO;
 import com.example.actualtravellerkiviprojectui.dto.PagedModel;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.util.List;
 
@@ -20,27 +21,33 @@ public class MockEventService implements EventService {
     @Override
     public Call<EventDTO> createEvent(EventDTO event) {
         // Mock a response
-        return delegate.returningResponse(Utils.loadMockJson("mock/events/event_1.json")).createEvent(event);
+        return delegate.returningResponse(Utils.loadObject("mock/events/event_1.json", new TypeReference<EventDTO>() {
+        })).createEvent(event);
     }
 
     @Override
     public Call<List<EventDTO>> getAllEvents() {
-        return delegate.returningResponse(Utils.loadMockJson("mock/events/event_list.json")).getAllEvents();
+        return delegate.returningResponse(Utils.loadObject("mock/events/event_list.json", new TypeReference<EventDTO>() {
+        })).getAllEvents();
     }
 
     @Override
     public Call<PagedModel<EventDTO>> getPaginatedEvents(int size, int page, String sort) {
-        return delegate.returningResponse(Utils.loadMockJson("mock/events/event_list.json")).getPaginatedEvents(size, page, sort);
+        List<EventDTO> mockEvents = Utils.loadObject("mock/events/event_list.json", new TypeReference<List<EventDTO>>() {
+        });
+        return delegate.returningResponse(mockEvents).getPaginatedEvents(size, page, sort);
     }
 
     @Override
     public Call<EventDTO> getEvent(int eventId) {
-        return delegate.returningResponse(Utils.loadMockJson("mock/events/event_" + eventId + ".json")).getEvent(eventId);
+        return delegate.returningResponse(Utils.loadObject("mock/events/event_" + eventId + ".json", new TypeReference<EventDTO>() {
+        })).getEvent(eventId);
     }
 
     @Override
     public Call<EventDTO> updateEvent(int eventId, EventCreateUpdate update) {
-        return delegate.returningResponse(Utils.loadMockJson("mock/events/event_" + eventId + ".json")).updateEvent(eventId, update);
+        return delegate.returningResponse(Utils.loadObject("mock/events/event_" + eventId + ".json", new TypeReference<EventDTO>() {
+        })).updateEvent(eventId, update);
     }
 
     @Override
@@ -48,7 +55,4 @@ public class MockEventService implements EventService {
         return delegate.returningResponse("").deleteEvent(eventId);
     }
 
-    public String loadMockJson(String fileName) {
-        return Utils.loadMockJson(fileName);
-    }
 }
