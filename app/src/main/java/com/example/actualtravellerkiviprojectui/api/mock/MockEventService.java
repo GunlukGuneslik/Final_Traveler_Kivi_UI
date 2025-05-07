48f613a11dd1c79c58fe5399641a14774851e51d
Yusuf Said Aktan <ysaktan@proton.me>
2025-05-07 16:02:57 +0300

    feat(Event): add mock EventService

--- app/src/main/java/com/example/actualtravellerkiviprojectui/api/mock/MockEventService.java
+++ app/src/main/java/com/example/actualtravellerkiviprojectui/api/mock/MockEventService.java
@@ -1,7 +1,13 @@
 package com.example.actualtravellerkiviprojectui.api.mock;
 
 import com.example.actualtravellerkiviprojectui.api.EventService;
+import com.example.actualtravellerkiviprojectui.dto.EventCreateUpdate;
+import com.example.actualtravellerkiviprojectui.dto.EventDTO;
+import com.example.actualtravellerkiviprojectui.dto.PagedModel;
 
+import java.util.List;
+
+import retrofit2.Call;
 import retrofit2.mock.BehaviorDelegate;
 
 public class MockEventService implements EventService {
@@ -9,5 +15,66 @@ public class MockEventService implements EventService {
 
     public MockEventService(BehaviorDelegate<EventService> delegate) {
         this.delegate = delegate;
+    }
+
+    /**
+     * Creates new event.
+     *
+     * @param event
+     */
+    @Override
+    public Call<EventDTO> createEvent(EventDTO event) {
+        return null;
+    }
+
+    /**
+     * Returns the list of all events.
+     */
+    @Override
+    public Call<List<EventDTO>> getAllEvents() {
+        return null;
+    }
+
+    /**
+     * Returns paginated list of events.
+     *
+     * @param size
+     * @param page
+     * @param sort
+     */
+    @Override
+    public Call<PagedModel<EventDTO>> getPaginatedEvents(int size, int page, String sort) {
+        return null;
+    }
+
+    /**
+     * Gets an event according to id.
+     *
+     * @param eventId
+     */
+    @Override
+    public Call<EventDTO> getEvent(int eventId) {
+        return null;
+    }
+
+    /**
+     * Updates an event.
+     *
+     * @param eventId
+     * @param update
+     */
+    @Override
+    public Call<EventDTO> updateEvent(int eventId, EventCreateUpdate update) {
+        return null;
+    }
+
+    /**
+     * Deletes an event.
+     *
+     * @param eventId
+     */
+    @Override
+    public Call<Void> deleteEvent(int eventId) {
+        return null;
     }
 }
