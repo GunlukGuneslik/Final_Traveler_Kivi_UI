decaacb76b486576070a08e3a98a7c3f8b00bf01
GünlükGüneşlik <caglagunesocakli@gmail.com>
2025-05-08 21:46:45 +0300

    Biraz daha özellik

--- app/src/main/java/com/example/actualtravellerkiviprojectui/LaunchTourCreateActivity.java
+++ app/src/main/java/com/example/actualtravellerkiviprojectui/LaunchTourCreateActivity.java
@@ -1,14 +1,57 @@
 package com.example.actualtravellerkiviprojectui;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class LaunchTourCreateActivity extends AppCompatActivity {
+
+    private Button returnButton;
+    private Button selectDateButton;
+    private int year; private int month; private int day; // these are going to be used for creating the tour object
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_tour_create);
+
+        returnButton = findViewById(R.id.CreateNewTourPageReturnButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        selectDateButton = findViewById(R.id.CreateTourPageSelectDatePage);
+        selectDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+
+    private void openDialog() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+            }
+        }, year, month + 1, day);
+
+        dialog.show();
     }
 }
