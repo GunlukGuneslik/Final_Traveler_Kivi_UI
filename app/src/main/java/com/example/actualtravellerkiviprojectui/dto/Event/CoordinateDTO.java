e87fd64d0d8c4e9b1097d5ffd650e8a114fcb69d
Yusuf Said Aktan <ysaktan@proton.me>
2025-05-12 20:13:31 +0300

    fix(jackson): add empty constructor for CoordinateDTO

--- app/src/main/java/com/example/actualtravellerkiviprojectui/dto/Event/CoordinateDTO.java
+++ app/src/main/java/com/example/actualtravellerkiviprojectui/dto/Event/CoordinateDTO.java
@@ -7,6 +7,9 @@ public class CoordinateDTO {
     public double latitude;
     public double longtitude;
 
+    public CoordinateDTO() {
+
+    }
     public CoordinateDTO(double longtitude, double latitude) {
         this.longtitude = longtitude;
         this.latitude = latitude;
