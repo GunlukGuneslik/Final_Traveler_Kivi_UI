4ac0df128a2aa0777d6f00aeabe0843c1ffa5e06
GünlükGüneşlik <caglagunesocakli@gmail.com>
2025-05-13 16:42:37 +0300

    Edit sayfasına uğraşıypruz

--- app/src/main/java/com/example/actualtravellerkiviprojectui/EditTourNoteFragment.java
+++ app/src/main/java/com/example/actualtravellerkiviprojectui/EditTourNoteFragment.java
@@ -1,5 +1,91 @@
 package com.example.actualtravellerkiviprojectui;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
 import androidx.fragment.app.Fragment;
 
+import com.example.actualtravellerkiviprojectui.dto.Event.EventDTO;
+
 public class EditTourNoteFragment extends Fragment {
+    EditTourActivity activity;
+    String tourDescription;
+    EditText editText;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public EditTourNoteFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MapPageFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static EditTourNoteFragment newInstance(String param1, String param2) {
+        EditTourNoteFragment fragment = new EditTourNoteFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_edit_tour_edit_tour_note, container, false);
+
+        activity = (EditTourActivity) getActivity();
+
+        tourDescription = activity.tourDescription;
+        editText = view.findViewById(R.id.EditTextEditTourNotes);
+
+        if (tourDescription != null) {
+            editText.setText(tourDescription);
+        }
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                tourDescription = s.toString();
+                activity.tourDescription = tourDescription;
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        return view;
+    }
 }
