5fe0a32e4cf71d4398c6abe35fb8763fb1275b93
Yusuf Said Aktan <ysaktan@proton.me>
2025-05-14 06:04:05 +0300

    feat(AccountPageFragment, Account_Page_Posts_RecyclerViewAdapter): refactor to use List for posts and simplify newInstance method

--- app/src/main/java/com/example/actualtravellerkiviprojectui/api/UserService.java
+++ app/src/main/java/com/example/actualtravellerkiviprojectui/api/UserService.java
@@ -62,7 +62,7 @@ public interface UserService {
 
     @POST("users/{userId}/avatar")
     @Multipart
-    Call<UserDTO> setAvatar(@Path("userId") int userId, @Part("image") MultipartBody.Part image);
+    Call<UserDTO> setAvatar(@Path("userId") int userId, @Part MultipartBody.Part image);
 
     @GET("users/{userId}/stats")
     Call<UserStatsDTO> getUserStats(@Path("userId") int userId);
